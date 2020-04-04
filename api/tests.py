75d9993e5e11e43bc32706765bbaadda5822ff8b
Ksenofanex <ksenofanex@gmail.com>
2020-04-04 14:53:19 +0300

    added tests for api

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from dictionary.models import Word
 
-# Create your tests here.
+
+class MaterialTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        testuser1 = get_user_model().objects.create_user(
+            username='testuser1', password='deneme123',
+        )
+        testuser1.save()
+
+        word = Word.objects.create(english='Computer', author=testuser1, turkish='Kompitür')
+        word.save()
+
+    def test_word(self):
+        word = Word.objects.get(id=1)
+        english = f"{word.english}"
+        author = f"{word.author.username}"
+        turkish = f"{word.turkish}"
+        self.assertEqual(english, 'Computer')
+        self.assertEqual(author, 'testuser1')
+        self.assertEqual(turkish, 'Kompitür')
