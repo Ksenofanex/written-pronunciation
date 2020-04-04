e7026e95d3aaf6d9ff0375d188657acb8b72b0c2
Ksenofanex <ksenofanex@gmail.com>
2020-04-04 12:54:33 +0300

    added tests for homepage

--- dictionary/tests.py
+++ dictionary/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTests(TestCase):
+
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, 'dictionary/home.html')
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, 'Hi there! I should not be on this page.')
