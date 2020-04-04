5ee052c18d814db882d3a8d1ed35e412b32731bb
Ksenofanex <ksenofanex@gmail.com>
2020-04-04 12:49:34 +0300

    added tests for users app

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,53 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='hedaelus',
+            email='hedaelus@gmail.com',
+            password='test',
+        )
+        self.assertEqual(user.username, 'hedaelus')
+        self.assertEqual(user.email, 'hedaelus@gmail.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username='superadmin',
+            email='superadmin@admin.com',
+            password='admin123'
+        )
+        self.assertEqual(admin_user.username, 'superadmin')
+        self.assertEqual(admin_user.email, 'superadmin@admin.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+
+
+class SignupTests(TestCase):
+
+    username = 'newuser'
+    email = 'newuser@email.com'
+
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'signup.html')
+        self.assertContains(self.response, 'Sign Up')
+        self.assertNotContains(self.response, 'Hi there! I should not on this page.')
+
+    def test_signup_form(self):
+        new_user = get_user_model().objects.create_user(
+            self.username)
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, self.username)
