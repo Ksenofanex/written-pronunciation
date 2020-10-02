20974c1455aa327ab8c7669f5acbc6f8c8f8f3d9
Burak Kadir Er <ksenofanex@gmail.com>
2020-10-02 13:20:52 +0300

    Update views.py

--- api/views.py
+++ api/views.py
@@ -8,6 +8,10 @@ from .permissions import IsAuthorOrReadOnly
 class WordList(generics.ListCreateAPIView):
     queryset = Word.objects.all()
     serializer_class = WordSerializer
+    
+    def perform_create(self, serializer): # For setting the author field currently logged-in user.
+    serializer.validated_data['author'] = self.request.user
+    return super(WordList, self).perform_create(serializer)
 
 
 class WordDetail(generics.RetrieveUpdateDestroyAPIView):
