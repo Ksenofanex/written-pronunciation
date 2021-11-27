6dccae6bb52cef21bf546ba403d3e90b4de947d1
Burak Kadir Er <ksenofanex@gmail.com>
2021-11-27 12:44:21 +0300

    Update forms.py

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
+
+from users.models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
