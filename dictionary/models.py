992f392f43d826c2a2cffa4696d705146538c219
Burak Kadir Er <ksenofanex@gmail.com>
2022-08-04 11:49:44 +0300

    Give final shape to Word model and search bar

--- dictionary/models.py
+++ dictionary/models.py
@@ -1,18 +1,25 @@
-from django.contrib.auth import get_user_model
+from django.conf import settings
 from django.db import models
 from django.urls import reverse
 
 
 class Word(models.Model):
-    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    english = models.CharField(max_length=250)
-    turkish = models.CharField(max_length=250)
+    author = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="words",
+        related_query_name="word",
+    )
+    english = models.CharField(verbose_name="english word", max_length=250)
+    turkish = models.CharField(verbose_name="turkish word", max_length=250)
+
+    class Meta:
+        verbose_name = "Word"
+        verbose_name_plural = "Words"
+        ordering = ["english"]
 
     def __str__(self):
-        return self.english  # Human readable object name.
+        return self.english
 
     def get_absolute_url(self):
-        return reverse("word_detail", args=[str(self.id)])
-
-    # The get_absolute_url() method sets a canonical URL for the model.
-    # It is also the correct way to refer to a model in your templates rather than hard-coding them.
+        return reverse("word-detail", args=[str(self.id)])
