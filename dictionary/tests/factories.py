4bada2e4bc1f893ef6ae231d1bb1449e94d68871
Burak Kadir Er <ksenofanex@gmail.com>
2022-08-11 00:16:32 +0300

    Prevent unique constraint error in WordFactory

--- dictionary/tests/factories.py
+++ dictionary/tests/factories.py
@@ -7,6 +7,8 @@ from users.tests.factories import UserFactory
 class WordFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Word
+        django_get_or_create = ("english",)  # To prevent unique constraint
+        # error.
 
     english = factory.Faker("word")
     turkish = "Fektri"
